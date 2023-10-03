ad0a0f139d88d0fd80818980288b81b018c6af6e
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-03 19:09:29 -0300

    feat: implement methods of ClienteController

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cliente;
+use App\Repositories\ClienteRepository;
 use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
+    public function __construct(private Cliente $cliente)
+    {
+    }
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $clienteRepository = new ClienteRepository($this->cliente);
+        if ($request->has('filtro')) $clienteRepository->filtro($request->filtro);
+        if ($request->has('atributos')) $clienteRepository->selectAtributos($request->atributos);
+        return $clienteRepository->getResultado();
     }
 
     /**
@@ -20,30 +28,50 @@ class ClienteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->cliente->rules(), $this->cliente->feedback());
+        $cliente = $this->cliente->create($request->all());
+        return $cliente;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Cliente $cliente)
+    public function show(Int $id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+        if (empty($cliente)) return response()->json(['erro' => 'Recurso pesquisado não existe'], 404);
+        return $cliente;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Cliente $cliente)
+    public function update(Request $request, Int $id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+        if (empty($cliente)) return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe'], 404);
+        if ($request->isMethod('patch')) {
+            $rules = array();
+            foreach ($cliente->rules($id) as $input => $regras) {
+                if (array_key_exists($input, $request->all())) $rules[$input] = $regras;
+            }
+            $request->validate($rules, $this->cliente->feedback());
+        } else {
+            $request->validate($this->cliente->rules($id), $this->cliente->feedback());
+        }
+        $cliente->fill($request->all());
+        $cliente->save();
+        return $cliente;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Cliente $cliente)
+    public function destroy(Int $id)
     {
-        //
+        $cliente = $this->cliente->find($id);
+        if (empty($cliente)) return response()->json(['erro' => 'Impossível realizar a exclusão. O recurso solicitado não existe'], 404);
+        $cliente->delete();
+        return ['msg' => 'O cliente foi removido com sucesso!'];
     }
 }
--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nome'];
+
+    public function rules($id = null)
+    {
+        return [
+            'nome' => "required|unique:clientes,nome,{$id}",
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.unique' => 'O nome informado já existe',
+        ];
+    }
+
 }
