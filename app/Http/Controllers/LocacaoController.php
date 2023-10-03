f17f9e16a547af474db436a6723e3f469abaed5e
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-03 19:59:19 -0300

    feat: implement methods of LocacaoController

--- app/Http/Controllers/LocacaoController.php
+++ app/Http/Controllers/LocacaoController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Locacao;
+use App\Repositories\LocacaoRepository;
 use Illuminate\Http\Request;
 
 class LocacaoController extends Controller
 {
+    public function __construct(private Locacao $locacao)
+    {
+    }
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $locacaoRepository = new LocacaoRepository($this->locacao);
+        if ($request->has('filtro')) $locacaoRepository->filtro($request->filtro);
+        if ($request->has('atributos')) $locacaoRepository->selectAtributos($request->atributos);
+        return $locacaoRepository->getResultado();
     }
 
     /**
@@ -20,30 +28,50 @@ class LocacaoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->locacao->rules(), $this->locacao->feedback());
+        $locacao = $this->locacao->create($request->all());
+        return $locacao;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Locacao $locacao)
+    public function show(Int $id)
     {
-        //
+        $locacao = $this->locacao->find($id);
+        if (empty($locacao)) return response()->json(['erro' => 'Recurso pesquisado não existe'], 404);
+        return $locacao;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Locacao $locacao)
+    public function update(Request $request, Int $id)
     {
-        //
+        $locacao = $this->locacao->find($id);
+        if (empty($locacao)) return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe'], 404);
+        if ($request->isMethod('patch')) {
+            $rules = array();
+            foreach ($locacao->rules($id) as $input => $regras) {
+                if (array_key_exists($input, $request->all())) $rules[$input] = $regras;
+            }
+            $request->validate($rules, $this->locacao->feedback());
+        } else {
+            $request->validate($this->locacao->rules($id), $this->locacao->feedback());
+        }
+        $locacao->fill($request->all());
+        $locacao->save();
+        return $locacao;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Locacao $locacao)
+    public function destroy(Int $id)
     {
-        //
+        $locacao = $this->locacao->find($id);
+        if (empty($locacao)) return response()->json(['erro' => 'Impossível realizar a exclusão. O recurso solicitado não existe'], 404);
+        $locacao->delete();
+        return ['msg' => 'A locação foi removido com sucesso!'];
     }
 }
--- app/Models/Locacao.php
+++ app/Models/Locacao.php
@@ -9,8 +9,39 @@ class Locacao extends Model
 {
     use HasFactory;
     protected $table = 'locacoes';
-}
+    protected $fillable = [
+        'cliente_id',
+        'carro_id',
+        'data_inicio_periodo',
+        'data_final_previsto_periodo',
+        'data_final_realizado_periodo',
+        'valor_diaria',
+        'km_inicial',
+        'km_final',
+    ];
 
-Route::get('/', function () {
-    return ['Chegamos até aqui' => 'SIM'];
-});
+    public function rules($id = null)
+    {
+        return [
+            'cliente_id' => 'required|exists:clientes,id',
+            'carro_id' => 'required|exists:carros,id',
+            'data_inicio_periodo' => 'required|date_format:Y/m/d',
+            'data_final_previsto_periodo' => 'required|date_format:Y/m/d',
+            'data_final_realizado_periodo' => 'date_format:Y/m/d',
+            'valor_diaria' => 'required|decimal:2',
+            'km_inicial' => 'required'
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'cliente_id.exists' => 'Cliente não encontrado',
+            'carro_id.exists' => 'Carro não encontrado',
+            'integer' => 'O campo :attribute deve ser um número inteiro',
+            'date_format' => 'O campo :attribute deve uma data válida',
+            'decimal' => 'O campo :attribute deve ser um decimal (0.00)',
+        ];
+    }
+}
