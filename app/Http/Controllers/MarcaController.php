5f2ffa0a9ab7487af74b36ea7ffa6a7f40cecf8d
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-03 00:44:20 -0300

    feat: implement methods of MarcaController

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -4,15 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Marca;
 use Illuminate\Http\Request;
+use Ramsey\Uuid\Type\Integer;
 
 class MarcaController extends Controller
 {
+    public function __construct(public Marca $marca)
+    {
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $marcas = $this->marca->all();
+        return $marcas;
     }
 
     /**
@@ -20,30 +26,36 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $marca = $this->marca->create($request->all());
+        return $marca;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Marca $marca)
+    public function show(Int $id)
     {
-        //
+        $marca = $this->marca->find($id);
+        return $marca;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, Int $id)
     {
-        //
+        $marca = $this->marca->find($id);
+        $marca->update($request->all());
+        return $marca;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Marca $marca)
+    public function destroy(Int $id)
     {
-        //
+        $marca = $this->marca->find($id);
+        $marca->delete();
+        return ['msg' => 'A marca foi removida com sucesso!'];
     }
 }
